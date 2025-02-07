c5979846cf78983402656f0619fb00bc3e4d8e7b
Rythmat <teotaniwha@gmail.com>
2025-02-07 10:48:23 -0700

    Router Configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Routes, Route} from "react-router-dom";
 
 function App() {
 
@@ -7,7 +8,13 @@ function App() {
      <div id="container">
       <h1>Hello React Router!</h1>
       <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      <div id="main-section">
+        <Routes>
+          <Route path='/' element={<h2>Welcome!</h2>}/>
+          <Route path='/blue' element={<h2>Blue!</h2>}/>
+          <Route path='/red' element={<h2>Red!</h2>}/>
+        </Routes>
+      </div>
     </div>
     </>
   )
